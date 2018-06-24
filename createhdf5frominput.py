3721ca0bb9f8eb7c00890aad5385c18edbf28865
Evan Pratten <ewpratten@retrylife.ca>
2018-06-24 15:49:19 -0400

    [gitbot] autocommit

--- createhdf5frominput.py
+++ createhdf5frominput.py
@@ -1,11 +1,14 @@
 from textgenrnn import textgenrnn
 import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-e", "--num-epochs", type=int, default=100, help="Number epochs for generation")
-ap.add_argument("-i", "--input-file", type=str, default="./input.txt", help="Which file to grab input data from")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-e", "--num-epochs", type=int, default=100, help="Number epochs for generation")
+# ap.add_argument("-i", "--input-file", type=str, default="./input.txt", help="Which file to grab input data from")
+# args = vars(ap.parse_args())
+
+imput_file = "./input.txt"
+epochs = 100
 
 textgen = textgenrnn()
 
-textgen.train_from_file(args["input-file"], num_epochs=args["num-epochs"])
+textgen.train_from_file(input_file, num_epochs=epochs)
--- createsomerants.py
+++ createsomerants.py
@@ -1,11 +1,13 @@
 from textgenrnn import textgenrnn
 import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input-file", type=str, default="./Checkpoint\ Files/Linuxxx-epoch-90.hdf5", help="Which hdf5 file to grab input data from")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--input-file", type=str, default="./Checkpoint\ Files/Linuxxx-epoch-90.hdf5", help="Which hdf5 file to grab input data from")
+# args = vars(ap.parse_args())
 
-textgen_2 = textgenrnn(args["input-file"])
+input_file = "./Checkpoint\ Files/Linuxxx-epoch-90.hdf5"
+
+textgen_2 = textgenrnn(input_file)
 
 while True:
     textgen_2.generate(3, temperature=1.0)
